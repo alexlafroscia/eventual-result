14624f4e864e1709e2d78f1cfaaee7d362b3df03
Alex LaFroscia <alex@lafroscia.com>
2022-03-03 09:49:43 -0500

    add script to generate `npm` package

---
+++ scripts/build-npm.ts
@@ -0,0 +1,36 @@
+import { build, emptyDir } from "https://deno.land/x/dnt/mod.ts";
+
+// Ensure `npm` directory is empty
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./lib/mod.ts"],
+  outDir: "./npm",
+  shims: {
+    // see JS docs for overview and more options
+    deno: true,
+  },
+  // Does not feel necessary, given there are no dependencies the tool needs to swap
+  test: false,
+  // Disable type-checking of the output lib; can't get `Error#cause` working
+  typeCheck: false,
+  // package.json properties
+  package: {
+    name: "eventual-result",
+    version: Deno.args[0],
+    description:
+      "A `Result` and `Option` implementation that works with `Promise`",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/alexlafroscia/eventual-result.git",
+    },
+    bugs: {
+      url: "https://github.com/alexlafroscia/eventual-result/issues",
+    },
+  },
+});
+
+// post build steps
+Deno.copyFileSync("LICENSE", "npm/LICENSE");
+// Deno.copyFileSync("README.md", "npm/README.md");
