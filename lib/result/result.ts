fa3ab18285be16224552db28648ca235dc2cb60f
Alex LaFroscia <alex@lafroscia.com>
2022-03-10 15:50:39 -0500

    docs: adding better `Result` docs

--- lib/result/result.ts
+++ lib/result/result.ts
@@ -1,4 +1,12 @@
 import { OkImpl } from "./ok.ts";
 import { ErrImpl } from "./err.ts";
 
+/**
+ * Represents either a successful value (`OkImpl<T>`) or an error (`ErrImpl<E>`)
+ *
+ * See [`ResultMethods`]{@link import('./methods.ts').ResultMethods} for method documentation
+ *
+ * @template T The type of the successful value that the `Result` could contain
+ * @template E The type of the error value that the `Result` could contain
+ */
 export type Result<T, E> = OkImpl<T> | ErrImpl<E>;
