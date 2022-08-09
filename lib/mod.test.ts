4289ce18d5db10bf5238168b41a4783b6b8a0e2f
Alex LaFroscia <alex@lafroscia.com>
2022-08-09 14:48:54 -0400

    add test for module exports

---
+++ lib/mod.test.ts
@@ -0,0 +1,24 @@
+import { assertExists } from "./test-deps.ts";
+import * as mod from "./mod.ts";
+
+Deno.test("module exports", () => {
+  assertExists(mod.EventualResult, "Exports `EventualResult`");
+
+  assertExists(mod.Ok, "Exports `Ok`");
+  assertExists(mod.Err, "Exports `Err`");
+
+  assertExists(mod.isOk, "Exports `isOk`");
+  assertExists(mod.isErr, "Exports `isErr`");
+
+  assertExists(mod.anyResult, "Exports `anyResult`");
+  assertExists(mod.allResults, "Exports `allResults`");
+
+  assertExists(mod.Some, "Exports `Some`");
+  assertExists(mod.None, "Exports `None`");
+
+  assertExists(mod.isSome, "Exports `isSome`");
+  assertExists(mod.isNone, "Exports `isNone`");
+
+  assertExists(mod.UnwrapError, "Exports `UnwrapError`");
+  assertExists(mod.ExpectError, "Exports `ExpectError`");
+});
